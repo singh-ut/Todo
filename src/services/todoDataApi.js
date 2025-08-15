896e55aabb5b78f2bd82fb3b6d913d342800b98c
Utkarsh Singh Tomar <devutkarsh.codes@gmail.com>
2025-08-16 01:08:24 +0530

    Changed baseUrl

--- src/services/todoDataApi.js
+++ src/services/todoDataApi.js
@@ -2,7 +2,7 @@ import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
 
 export const todoDataApi = createApi({
     reducerPath: "todoDataApi",
-    baseQuery: fetchBaseQuery({baseUrl: 'http://localhost:8080/'}),
+    baseQuery: fetchBaseQuery({baseUrl: 'https://todo-backend-mtln.onrender.com'}),
     tagTypes: ['Todos'],
     endpoints: (builder) => ({
         getAllTodos: builder.query({
